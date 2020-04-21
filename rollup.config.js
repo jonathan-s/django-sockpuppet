2066c446fead3a600bd66bbfbc6d7b184d39326d
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-04-21 15:08:04 +0200

    Find all reflex classes

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from "rollup-plugin-babel"
 import { terser } from "rollup-plugin-terser"
 import resolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
+
 
 const options = {
   mangle: false,
--- sockpuppet/consumer.py
+++ sockpuppet/consumer.py
@@ -1,6 +1,9 @@
-import json
-import logging
 
+import logging
+from importlib import import_module
+from os import walk
+
+from django.apps import apps
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import JsonWebsocketConsumer
 
@@ -10,12 +13,45 @@ logger = logging.getLogger('sockpuppet')
 
 class SockpuppetConsumer(JsonWebsocketConsumer):
     channel_name = ''
+    reflexes = []
+
+    def load_reflexes_from_config(self, config):
+        def append_reflex(module):
+            for classname in dir(module):
+                if 'reflex' in classname.lower():
+                    ReflexClass = getattr(module, classname)
+                    self.reflexes.append(ReflexClass)
+
+        path = config.module.__path__[0]
+        for dirpath, dirnames, filenames in walk(path):
+            if dirpath == path and 'reflexes' in dirnames:
+                # classes in reflexes.py
+                import_path = '{}.reflexes'.format(config.name)
+                module = import_module(import_path)
+                append_reflex(module)
+            elif dirpath == '{}/{}'.format(path, 'reflexes'):
+                # assumes reflexes folder is placed directly in app.
+                import_path = '{config_name}.reflexes.{reflex_file}'
+                for filename in filenames:
+                    name = filename.split('.')[0]
+                    full_import_path = import_path.format(
+                        config_name=config.name, reflex_file=name
+                    )
+                    module = import_module(full_import_path)
+                    append_reflex(module)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        logger.debug('initialize')
-        logger.info('test info')
         self.subscriptions = set()
+
+        if not self.reflexes:
+            configs = apps.app_configs.values()
+            for config in configs:
+                self.load_reflexes_from_config(config)
+
+    def receive_json(self, content, **kwargs):
+        logger.debug('Json: %s', content)
+        logger.debug('kwargs: %s', kwargs)
 
     def subscribe(self, event):
         room_name = event['room_name']
@@ -52,4 +88,3 @@ class SockpuppetConsumer(JsonWebsocketConsumer):
         for room in list(self.subscriptions):
             self.unsubscribe({'room_name': room})
         logger.debug(f':: DISCONNECT {self.channel_name}')
-
