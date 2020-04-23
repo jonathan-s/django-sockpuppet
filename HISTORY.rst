eea85bdf26f7537c767168f70cc64fe43e64d94f
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-04-23 20:16:58 +0200

    Some clean up of things that won't be used

--- HISTORY.rst
+++
@@ -1,9 +0,0 @@
-.. :changelog:
-
-History
--------
-
-0.1.0 (2020-01-11)
-++++++++++++++++++
-
-* First release on PyPI.
--- sockpuppet/channel.py
+++ sockpuppet/channel.py
@@ -1,6 +1,6 @@
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from .utils import camelize, deep_transform
+from .utils import camelize
 
 
 class Channel:
