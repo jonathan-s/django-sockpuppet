ae0e79d83b93a0f20fef8a64b9426beffdb2b771
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-04-23 14:30:39 +0200

    No exceptions being thrown

--- sockpuppet/utils.py
+++ sockpuppet/utils.py
@@ -6,3 +6,9 @@ inflector = Inflector()
 def camelize(word):
     word = inflector.camelize(word)
     return '{}{}'.format(word[0].lower(), word[1:])
+
+
+def classify(word):
+    tail = camelize(word[1:])
+    head = word[:1].title()
+    return '{}{}'.format(head, tail)
