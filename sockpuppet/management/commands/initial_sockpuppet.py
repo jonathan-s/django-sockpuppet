523e454279e3ddd123d8056e59dd4de3aeb3fe1c
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-05-02 19:49:09 +0200

    Catch error and output warning if build/watch already in package.json

--- sockpuppet/management/commands/initial_sockpuppet.py
+++ sockpuppet/management/commands/initial_sockpuppet.py
@@ -1,6 +1,7 @@
 import subprocess
 
 from django.template.loader import get_template
+from subprocess import CalledProcessError
 
 from ._base import BaseGenerateCommand
 
@@ -10,11 +11,16 @@ class Command(BaseGenerateCommand):
 
     def handle(self, *args, **options):
         install = 'npm install -g add-project-script'
-        build = 'add-project-script -n "build" -v "webpack --mode production"'
-        watch = 'add-project-script -n "watch" -v "webpack --watch --info-verbosity verbose"'
         subprocess.check_call(install, shell=True)
-        subprocess.check_call(build, shell=True)
-        subprocess.check_call(watch, shell=True)
+        try:
+            build = 'add-project-script -n "build" -v "webpack --mode production"'
+            watch = 'add-project-script -n "watch" -v "webpack --watch --info-verbosity verbose"'
+            subprocess.check_call(build, shell=True)
+            subprocess.check_call(watch, shell=True)
+        except CalledProcessError:
+            msg = 'Build and watch already in package.json, so skipping these'
+            self.call_stdout(msg)
+
         subprocess.check_call('npm uninstall -g add-project-script', shell=True)
 
         npm_pkg = 'npm install -save-dev glob sockpuppet-js stimulus_reflex webpack webpack-cli'
@@ -23,5 +29,4 @@ class Command(BaseGenerateCommand):
         template = get_template('sockpuppet/scaffolds/webpack.html')
         rendered = template.render({})
         self.create_file('', 'webpack.config.js', rendered)
-
         self.call_stdout('Scaffolding generated!', _type='SUCCESS')
