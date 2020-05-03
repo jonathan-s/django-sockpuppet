e092afe2d46ed084744aa551597d4e22d54bc2e3
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-05-03 22:46:45 +0200

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 import re
-import sys
 
 try:
     from setuptools import setup
@@ -22,25 +21,6 @@ def get_version(*file_paths):
 
 
 version = get_version("sockpuppet", "__init__.py")
-
-
-if sys.argv[-1] == 'publish':
-    try:
-        import wheel
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
-    sys.exit()
-
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
-    sys.exit()
-
 readme = open('README.md').read()
 requirements = open('requirements.txt').readlines()
 
