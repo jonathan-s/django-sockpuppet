a91c657d1a50e3cb816e1d6c0127da74620fd738
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-05-03 16:07:05 +0200

    Update webpack configuration

--- tests/example/javascript/example.js
+++ tests/example/javascript/example.js
@@ -1,7 +1,7 @@
 import { Application } from 'stimulus'
 import StimulusReflex from 'stimulus_reflex'
 // because travis had issues with 'sockpuppet-js' we had to do this.
-import WebsocketConsumer from '../../../sockpuppet/static/js/reflex-websocket'
+import WebsocketConsumer from '../../../javascript/stimulus-websocket/index'
 import ExampleController from './controllers/example_controller'
 
 const application = Application.start()
