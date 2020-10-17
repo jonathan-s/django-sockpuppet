cfcfa16fdc16bf3152b066c3e5e89781ac7bd808
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-10-17 21:53:22 +0200

    Cypress test that checks a reflex without controller

--- tests/example/reflexes/example_reflex.py
+++ tests/example/reflexes/example_reflex.py
@@ -4,3 +4,8 @@ from sockpuppet.reflex import Reflex
 class ExampleReflex(Reflex):
     def increment(self, step=1):
         self.session['count'] = int(self.element.dataset['count']) + step
+
+
+class DecrementReflex(Reflex):
+    def decrement(self, step=1):
+        self.session['otherCount'] = int(self.element.dataset['count']) - step
--- tests/settings.py
+++ tests/settings.py
@@ -6,13 +6,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "a_not_so_secret_key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition
