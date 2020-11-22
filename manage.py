ca76a79b8b29fd7a79c980daebf99e5e4a7ad73e
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-11-22 21:47:42 +0100

    Get coverage from dev server

--- manage.py
+++ manage.py
@@ -8,5 +8,4 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
