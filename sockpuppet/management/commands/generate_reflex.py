ac4c6fc0553927428311ddbc5362f4cfc9785d8c
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-12-25 23:42:11 +0100

    Generate a simpler setup by default for a smoother onboarding

--- sockpuppet/management/commands/generate_reflex.py
+++ sockpuppet/management/commands/generate_reflex.py
@@ -28,25 +28,41 @@ class Command(BaseGenerateCommand):
             default='example'
         )
 
+        parser.add_argument(
+            '--javascript',
+            dest='javascript',
+            action='store_true',
+            help='Include this to generate a setup than includes javascript with controllers'
+        )
+        parser.set_defaults(javascript=False)
+
     def handle(self, *args, **options):
         app_name = options['app_name'][0]
         reflex_name = options['reflex_name']
+        using_javascript = options['javascript']
 
         module_path = self.lookup_app_path(app_name)
         self.module_path = Path(module_path)
 
         paths = [
-            ('reflexes', '_reflex.py'),
-            ('javascript', '.js'),
-            ('javascript/controllers', '_controller.js'),
-            ('views', '.py'),
-            ('templates', '.html')
+            (False, 'reflexes', '_reflex.py'),
+            (True, 'javascript', '.js'),
+            (True, 'javascript/controllers', '_controller.js'),
+            (False, 'views', '.py'),
+            (False, 'templates', '.html')
         ]
 
-        for path, suffix in paths:
+        for without_js, path, suffix in paths:
             template_name = TEMPLATES[suffix]
             template = get_template(template_name)
-            rendered = template.render({'class_name': classify(reflex_name), 'reflex_name': reflex_name})
+            rendered = template.render({
+                'class_name': classify(reflex_name),
+                'reflex_name': reflex_name,
+                'using_javascript': using_javascript
+            })
+            if without_js and not using_javascript:
+                # skipping these templates
+                continue
             self.create_file(path, '{}{}'.format(reflex_name.lower(), suffix), rendered)
 
         self.create_file('views', '__init__.py', '')
