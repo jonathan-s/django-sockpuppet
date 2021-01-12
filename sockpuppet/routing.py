747f2d1daa6e5c3b5c4fd003cef88075cdc89663
Kalil de Lima <kaozdl@gmail.com>
2021-01-12 15:23:39 -0300

    add http to router to be 3.0 compliant

--- sockpuppet/routing.py
+++ sockpuppet/routing.py
@@ -1,12 +1,17 @@
 from django.urls import re_path
+try:
+    from django.core.asgi import get_asgi_application
+except ImportError:
+    from channels.routing import get_default_application as get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from sockpuppet.consumer import SockpuppetConsumer
 
-socket_patterns = [re_path(r'ws/sockpuppet-sync', SockpuppetConsumer)]
+socket_patterns = [re_path(r'ws/sockpuppet-sync', SockpuppetConsumer.as_asgi())]
 
 application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
-        URLRouter(socket_patterns)
+        URLRouter(socket_patterns),
     ),
 })
