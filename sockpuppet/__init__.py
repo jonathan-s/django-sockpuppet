23cd9f6bcd60f23edbba92cf149788936109904d
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2021-01-16 19:48:01 +0100

    Make it easy to import the correct SockpuppetConsumer

--- sockpuppet/__init__.py
+++ sockpuppet/__init__.py
@@ -1,1 +1,9 @@
+import channels
+
+channels_version = channels.__version__.split('.')[0]
+if int(channels_version) >= 3:
+    from sockpuppet.consumer import SockpuppetConsumerAsgi as SockpuppetConsumer # noqa
+else:
+    from sockpuppet.consumer import SockpuppetConsumer # noqa
+
 __version__ = '0.4.2'
