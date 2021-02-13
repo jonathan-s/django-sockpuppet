ce7f16dd24fba49b144437ebd43d06256992977b
Christian González <christian.gonzalez@nerdocs.at>
2021-02-13 10:57:21 +0100

    validate reflex_name and keep it clear during scaffolding lifecycle

--- sockpuppet/templates/sockpuppet/scaffolds/application.js
+++ sockpuppet/templates/sockpuppet/scaffolds/application.js
@@ -1,10 +1,10 @@
 import { Application } from 'stimulus'
 import StimulusReflex from 'stimulus_reflex'
 import WebsocketConsumer from 'sockpuppet-js'
-import {{ reflex_name|title }}Controller from './controllers/{{ reflex_name|lower }}_controller'
+import {{ class_name }}Controller from './controllers/{{ reflex_name }}_controller'
 
 const application = Application.start()
 const consumer = new WebsocketConsumer(`ws://${window.location.host}/ws/sockpuppet-sync`)
 
-application.register("{{ reflex_name }}", {{ reflex_name|title }}Controller)
+application.register("{{ reflex_name }}", {{ class_name }}Controller)
 StimulusReflex.initialize(application, { consumer })
